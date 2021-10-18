8fdcc7c95e4eee4f2effa81122d1ba54e0b61b4b
dll31 <danila.litvinov31@yandex.ru>
2021-10-18 22:57:38 +0300

    Теперь ValueInRange находится в utility

--- labs/utility.cpp
+++ labs/utility.cpp
@@ -1,9 +1,29 @@
 
 #include "utility.h"
-
+#include <string>
 
 void repairCin()
 {
     std::cin.clear();
     std::cin.ignore(10000, '\n');
 }
+
+
+bool isValueInRange(const int & value, int from, int to)
+{
+    if ((value <= to) && (value >= from))
+        return true;
+
+    return false;
+}
+
+
+bool isValueInRange(const double & value, double from, double to)
+{
+    if ((value <= to) && (value >= from))
+        return true;
+
+    return false;
+}
+
+
--- labs/utility.h
+++ labs/utility.h
@@ -3,3 +3,7 @@
 #include <iostream>
 
 void repairCin();
+
+bool isValueInRange(const int& value, int from, int to);
+bool isValueInRange(const double& value, double from, double to);
+
