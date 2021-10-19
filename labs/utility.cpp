844caaaa7002a9225dad9db85c99e54eb08cae82
dll31 <danila.litvinov31@yandex.ru>
2021-10-19 13:21:58 +0300

    Перегрузка переделана на шаблон

--- labs/utility.cpp
+++ labs/utility.cpp
@@ -1,29 +1,9 @@
 
 #include "utility.h"
-#include <string>
+#include <string.h>
 
 void repairCin()
 {
     std::cin.clear();
     std::cin.ignore(10000, '\n');
 }
-
-
-bool isValueInRange(const int & value, int from, int to)
-{
-    if ((value <= to) && (value >= from))
-        return true;
-
-    return false;
-}
-
-
-bool isValueInRange(const double & value, double from, double to)
-{
-    if ((value <= to) && (value >= from))
-        return true;
-
-    return false;
-}
-
-
