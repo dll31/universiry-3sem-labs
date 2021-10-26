44ba3c849dabe171aefc12c002773c63923d7261
dll31 <danila.litvinov31@yandex.ru>
2021-10-26 17:20:49 +0300

    Добавлены функции ввода, получения id, редактирования, удаления для класса Network

--- labs/Network.h
+++ labs/Network.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <unordered_map>
 
 #include "Pipe.h"
 #include "Compressor_station.h"
@@ -13,8 +14,50 @@ private:
     std::string compStationSepInFile = "CS";
 
 public:
-    Pipe p;
-    Compressor_station cs;
+
+    std::unordered_map<int, Pipe> Pipeline;
+    std::unordered_map<int, Compressor_station> CSArray;
+
+    //Pipe p;
+    //Compressor_station cs;
 
     void loadElementsFromFile(std::string file);
+    template <typename T>
+    int getId(const T& map);
+
+    void pipeInputConsole();
+    void csInputConsole();
+
+    void pipeEdit(int id);
+    void csEdit(int id);
+
+    void pipeDeleteEnement(int id);
+    void csDeleteEnement(int id);
+
+    void display();
 };
+
+
+template <typename T>
+int getId(const T& map)
+{
+    int id = 0;
+
+    while (true)
+    {
+        //from example: https://www.geeksforgeeks.org/unordered_map-at-cpp/
+        try
+        {
+            map.at(id);
+        }
+
+        catch (const std::out_of_range& e)
+        {
+            return id;
+        }
+
+        id++;
+    }
+
+    return id;
+}
