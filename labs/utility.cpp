9cb57c80cc89fb46bd8a177f85ef3a538f362636
dll31 <danila.litvinov31@yandex.ru>
2021-10-28 11:22:14 +0300

    Функция для разбора ошибок при работе с файлами

--- labs/utility.cpp
+++ labs/utility.cpp
@@ -7,3 +7,28 @@ void repairCin()
     std::cin.clear();
     std::cin.ignore(10000, '\n');
 }
+
+
+void parseFileErrorCode(int errCode)
+{
+    if (errCode == 0)
+    {
+        std::cout << "Succesfull operation!\n";
+    }
+    else
+    {
+        if (errCode == -1)
+        {
+            std::cout << "Could not open file!\n";
+        }
+        else if (errCode == -2)
+        {
+            std::cout << "Broken data in file!\n";
+            //TODO: try to write exception in cout
+        }
+        else
+        {
+            std::cout << "Unexpected error code: " << errCode << '\n';
+        }
+    }
+}
